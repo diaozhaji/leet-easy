8e8a3b33bb909c6b0482e87fba87a070ed8b9ad2
jiyuan <jiyuan0371@gmail.com>
2019-03-18 20:18:02 +0800

    Mod 做了几道回溯的题

--- src/medium/Medium.java
+++ src/medium/Medium.java
@@ -47,7 +47,21 @@ public class Medium {
 //        }
 
         //22.
-        System.out.println(GenerateParentheses22.generateParenthesis(3));
+//        System.out.println(GenerateParentheses22.generateParenthesis(3));
+
+        //31.
+//        int[] nums = {1, 3, 2};
+//        NextPermutation31.nextPermutation(nums);
+//        for (int i = 0; i < nums.length; i++) {
+//            System.out.println(nums[i]);
+//
+//        }
+
+        //39.
+
+        int[] a = {2, 3, 6, 7};
+        System.out.println(CombinationSum39.combinationSum(a, 7));
+
 
     }
 }
